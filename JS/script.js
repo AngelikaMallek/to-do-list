6ba839a3930088207fdc585e9dcc2dcc7bac4ec6
Angelika Mallek <angelika.mallek03@gmail.com>
2023-02-23 19:39:45 +0100

    added possibility remove and add tasks

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,49 @@
 {
     const tasks = [];
 
+    const removeTasks = (index) => {
+        tasks.splice(index, 1);
+        render();
+    }
+
+    const toogleTaskDone = (index) => {
+        tasks[index].done = !tasks[index].done;
+        render();
+    }
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
-            htmlString += `<li class="section__listItem"><button class="section__listButton"></button>
+            htmlString += `<li class="section__listItem${task.done ? " section__listItem--done" : ""}">
+
+            <button class="section__listButton js-done"></button>
                 ${task.content}
-            <button class="section__listButton section__listButton--delete">✗</button></li>`;
+            <button class="section__listButton section__listButton--delete js-delete">✗</button>
+
+            </li>`;
         }
 
         document.querySelector(".js-list").innerHTML = htmlString;
+
+        const deleteButtons = document.querySelectorAll(".js-delete");
+        deleteButtons.forEach((deleteButton, index) => {
+            deleteButton.addEventListener("click", () => {
+                removeTasks(index);
+            });
+        })
+
+        const doneButtons = document.querySelectorAll(".js-done");
+        doneButtons.forEach((doneButton, index) => {
+            doneButton.addEventListener("click", () => {
+                toogleTaskDone(index);
+            });
+        })
     }
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-        const newTask = document.querySelector(".js-field").value;
+        const newTask = document.querySelector(".js-field").value.trim();
 
         if (!newTask) {
             return;
@@ -30,7 +58,6 @@
 
 
     const init = () => {
-
         const form = document.querySelector(".js-form");
         form.addEventListener("submit", onFormSubmit);
     }
